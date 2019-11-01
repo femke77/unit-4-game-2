ba732661f0d970636b0aa9ec592212d66530e37c
Meg Meyers <megan.g.meyers@gmail.com>
2019-10-31 23:14:48 -0700

    basic game logic working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,9 @@ var attack;
 var counterAtk;
 var dead;
 var attackFactor = 6;
+var characterArray = [];
+var player = null;
+var defender = null;
 
 
 $(document).ready(game() ); 
@@ -20,24 +23,83 @@ function CreateCharacter(hp, atk, catk) {
         this.attack = atk;
     }
     this.checkForDeath = function(hp){
-        if (hp <= 0){
+        if (hp <= 0) {
             this.dead = true;
         }
     }
-    
+
 }
 
 function game(){
-    var char1 = new CreateCharacter(100, 7, 5);
-    var char2 = new CreateCharacter(120, 10, 8);
-    var char3 = new CreateCharacter(150, 9, 7);
-    var char4 = new CreateCharacter(180, 6, 10);
-    
-    
+    characterArray[0] = new CreateCharacter(100, 7, 5);
+    characterArray[1] = new CreateCharacter(120, 10, 8);
+    characterArray[2] = new CreateCharacter(150, 9, 7);
+    characterArray[3] = new CreateCharacter(180, 6, 10);
+  
     
 }
 
+function checkIfDead(){
+    if (player.healthPoints <= 0){
+        player.dead = true;
+        console.log(player.dead)
+    }
+    if (defender.healthPoints <= 0){
+        defender.dead = true;
+        console.log(defender.dead)
+    }
+    if (player.dead){
+        // game over
+    } else if (defender.dead) {
+        //make defender disappear
+        if (!characterArray || !characterArray.length){        
+            defender = null;
+        }
+        
+    }
+}
 
+function setPlayerDefender(v){    
+    if (player == null){
+        player = characterArray[v];
+        //move player 
+        console.log(player);
+    } else if ((player != null) && (defender == null)) {
+        defender = characterArray[v];
+        //move defender
+        console.log(defender);
+    } 
+}
+
+$("#char1").on("click", function(){
+    var v = $("#char1").val();
+    setPlayerDefender(v);   
+});
+
+$("#char2").on("click", function(){
+    var v = $("#char2").val();
+    setPlayerDefender(v);
+});
+
+$("#char3").on("click", function(){
+    var v = $("#char3").val();
+    setPlayerDefender(v);
+});
+
+$("#char4").on("click", function(){
+    var v = $("#char4").val();
+    setPlayerDefender(v);
+});
+
+$("#attackBtn").on("click", function (){
+    defender.healthPoints -= player.attack;
+    console.log(defender.healthPoints)
+    player.healthPoints -= defender.counterAtk;
+    console.log(player.healthPoints)
+    checkIfDead();
+    
+
+});
 
 
 
