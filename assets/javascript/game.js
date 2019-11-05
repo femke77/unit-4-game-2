4664d931533f72106a23118bab4f1b7583a93f0a
Meg Meyers <megan.g.meyers@gmail.com>
2019-11-04 18:55:49 -0800

    additional game logic coded

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,107 +2,122 @@ var healthPoints;
 var attack;
 var counterAtk;
 var dead;
-var attackFactor = 6;
+var index;
+var attackFactor = 4;
 var characterArray = [];
 var player = null;
 var defender = null;
 
+$(document).ready(function () {
 
-$(document).ready(game() ); 
-
-function CreateCharacter(hp, atk, catk) {
-    this.healthPoints = hp;
-    this.attack = atk;
-    this.counterAtk = catk;
-    this.dead = false;
-    
-    this.changeHealth = function(hp){
+    function CreateCharacter(hp, atk, catk) {
         this.healthPoints = hp;
-    }
-    this.changeAttack = function(atk) {
         this.attack = atk;
+        this.counterAtk = catk;
+        this.dead = false;
+
+        this.changeHealth = function (hp) {
+            this.healthPoints = hp;
+        }
+        this.changeAttack = function (atk) {
+            this.attack = atk;
+        }
+        this.checkForDeath = function (hp) {
+            if (hp <= 0) {
+                this.dead = true;
+            }
+        }
+
     }
-    this.checkForDeath = function(hp){
-        if (hp <= 0) {
-            this.dead = true;
+
+    function game() {
+        characterArray.push(new CreateCharacter(100, 7, 5));
+        characterArray.push(new CreateCharacter(120, 9, 9));
+        characterArray.push(new CreateCharacter(150, 4, 7));
+        characterArray.push(new CreateCharacter(180, 6, 8));
+
+    }
+
+    function checkIfDead() {
+        if (player.healthPoints <= 0) {
+            player.dead = true;
+            console.log("player dead: " + player.dead)
+        }
+        if (defender.healthPoints <= 0) {
+            defender.dead = true;
+            console.log("defender dead: " + defender.dead)
+        }
+        if (player.dead) {
+            // game over logic here
+        } else if (defender.dead) {
+            //make defender disappear
+
+            if (characterArray.length > 0) {
+                console.log("pick new defender")
+                defender = null;
+
+            }
+
         }
     }
 
-}
+    //don't allow set to dead, or set to self
+    function setPlayerDefender(v) {
+        if (player == null) {
+            player = characterArray[v];
+            player.index = v;
 
-function game(){
-    characterArray[0] = new CreateCharacter(100, 7, 5);
-    characterArray[1] = new CreateCharacter(120, 10, 8);
-    characterArray[2] = new CreateCharacter(150, 9, 7);
-    characterArray[3] = new CreateCharacter(180, 6, 10);
-  
-    
-}
+            //move player jq
 
-function checkIfDead(){
-    if (player.healthPoints <= 0){
-        player.dead = true;
-        console.log(player.dead)
-    }
-    if (defender.healthPoints <= 0){
-        defender.dead = true;
-        console.log(defender.dead)
-    }
-    if (player.dead){
-        // game over
-    } else if (defender.dead) {
-        //make defender disappear
-        if (!characterArray || !characterArray.length){        
-            defender = null;
+            console.log(player);
+
+        } else if ((player != null) && (defender == null)) {
+            defender = characterArray[v];
+            defender.index = v;
+
+            //move defender jq
+
+            console.log(defender);
+            //move remaining characters (if any) to enemies area jq
         }
-        
     }
-}
 
-function setPlayerDefender(v){    
-    if (player == null){
-        player = characterArray[v];
-        //move player 
-        console.log(player);
-    } else if ((player != null) && (defender == null)) {
-        defender = characterArray[v];
-        //move defender
-        console.log(defender);
-    } 
-}
+    $("#char1").on("click", function () {
+        var v = $("#char1").val();
+        setPlayerDefender(v);
+    });
 
-$("#char1").on("click", function(){
-    var v = $("#char1").val();
-    setPlayerDefender(v);   
-});
+    $("#char2").on("click", function () {
+        var v = $("#char2").val();
+        setPlayerDefender(v);
+    });
 
-$("#char2").on("click", function(){
-    var v = $("#char2").val();
-    setPlayerDefender(v);
-});
+    $("#char3").on("click", function () {
+        var v = $("#char3").val();
+        setPlayerDefender(v);
+    });
 
-$("#char3").on("click", function(){
-    var v = $("#char3").val();
-    setPlayerDefender(v);
-});
+    $("#char4").on("click", function () {
+        var v = $("#char4").val();
+        setPlayerDefender(v);
+    });
 
-$("#char4").on("click", function(){
-    var v = $("#char4").val();
-    setPlayerDefender(v);
-});
 
-$("#attackBtn").on("click", function (){
-    defender.healthPoints -= player.attack;
-    console.log(defender.healthPoints)
-    player.healthPoints -= defender.counterAtk;
-    console.log(player.healthPoints)
-    checkIfDead();
-    
+    //disable this button at approprite times
+    $("#attackBtn").on("click", function () {
+        defender.healthPoints -= player.attack;
+        console.log(defender.healthPoints)
+        player.healthPoints -= defender.counterAtk;
+        console.log(player.healthPoints)
+        player.attack += attackFactor;
+        checkIfDead();
+
+
+    });
+
+
 
 });
-
-
-
 
 
 
